eb92a60e72daef37e838192f5e38fa78a8800575
MarcoBrasci1986 <marco_brasci@hotmail.com>
2016-11-11 11:42:12 +0100

    End Module 6

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,14 +1,23 @@
 import {NgModule} from "@angular/core";
 import {BrowserModule} from "@angular/platform-browser";
+import {FormsModule} from "@angular/forms";
 import {AppComponent} from "./app.component";
 import {ProductListComponent} from "./products/product-list.component";
 
 
 /**
+ * Main Entry point of the application:
+ * BrowserModule -> *ngIf, $ngFor
+ * FormsModule -> [(ngModel)]
+ *
+ * imports: external Angular Modules
  * declarations: add custom components
  */
 @NgModule({
-    imports: [BrowserModule],
+    imports: [
+        BrowserModule,
+        FormsModule
+    ],
     declarations: [
         AppComponent,
         ProductListComponent
