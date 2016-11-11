9501c9e084d96a0e7026fc1b1a3ceac488c7d3d0
MarcoBrasci1986 <marco_brasci@hotmail.com>
2016-11-11 09:31:51 +0100

    End Module 5

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,11 +1,19 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from "@angular/core";
+import {BrowserModule} from "@angular/platform-browser";
+import {AppComponent} from "./app.component";
+import {ProductListComponent} from "./products/product-list.component";
 
-import { AppComponent }  from './app.component';
 
+/**
+ * declarations: add custom components
+ */
 @NgModule({
-  imports: [ BrowserModule ],
-  declarations: [ AppComponent ],
-  bootstrap: [ AppComponent ]
+    imports: [BrowserModule],
+    declarations: [
+        AppComponent,
+        ProductListComponent
+    ],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
